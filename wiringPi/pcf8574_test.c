fcc69c51ebfbe38b0f68eb6440c26ae97be67d55
Rainer Poisel <rainer.poisel@gmail.com>
2014-02-05 09:56:30 +0100

    sample file using 8574 as output

--- wiringPi/pcf8574_test.c
+++ wiringPi/pcf8574_test.c
@@ -1,8 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <wiringPi.h>
+#include <pcf8574.h>
+
+#define BASE_8574 0x70
+#define ADDR_8574 0x38
+#define NUM_IO_8574 8
+
 int main(void)
 {
-    printf("Hello World!\n");
+    int cnt = 0;
+
+    wiringPiSetupSys();
+    pcf8574Setup(BASE_8574, ADDR_8574);
+
+    for (cnt = 0; cnt < NUM_IO_8574; cnt++)
+    {
+        pinMode(BASE_8574 + cnt, OUTPUT);
+    }
+
+    digitalWrite(BASE_8574, HIGH);
+
     return EXIT_SUCCESS;
 }
