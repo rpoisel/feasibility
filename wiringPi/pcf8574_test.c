b5834b39e9364e6af9c4124b5c29a392146ceb97
Rainer Poisel <rainer.poisel@gmail.com>
2014-02-05 11:00:39 +0100

    example demonstrating i2c capabilities completed

--- wiringPi/pcf8574_test.c
+++ wiringPi/pcf8574_test.c
@@ -5,22 +5,35 @@
 #include <pcf8574.h>
 
 #define BASE_8574 0x70
-#define ADDR_8574 0x38
+#define ADDR_I2C_8574 0x38
 #define NUM_IO_8574 8
 
 int main(void)
 {
     int cnt = 0;
+    unsigned level = LOW;
 
     wiringPiSetupSys();
-    pcf8574Setup(BASE_8574, ADDR_8574);
+    pcf8574Setup(BASE_8574, ADDR_I2C_8574);
 
     for (cnt = 0; cnt < NUM_IO_8574; cnt++)
     {
         pinMode(BASE_8574 + cnt, OUTPUT);
     }
 
-    digitalWrite(BASE_8574, HIGH);
+    for(;;)
+    {
+        level = LOW;
+        for (cnt = 0; cnt < NUM_IO_8574; cnt++)
+        {
+            digitalWrite(BASE_8574 + cnt, level);
+            if ((cnt + 1) % NUM_IO_8574 / 2 == 0)
+            {
+                level = (level == LOW) ? HIGH : LOW;
+            }
+        }
+        delay(500);
+    }
 
     return EXIT_SUCCESS;
 }
