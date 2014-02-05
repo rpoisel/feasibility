3c7e72ca86634c5a858550c870eca177b9fee7df
Rainer Poisel <rainer.poisel@gmail.com>
2014-02-05 14:48:13 +0100

    initial i2c project

---
+++ wiringPi/i2c/i2c_test.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <wiringPi.h>
+
+#define BASE_8574 0x70
+#define ADDR_I2C_8574 0x38
+
+int main(void)
+{
+    printf("Hello World!\n");
+    return EXIT_SUCCESS;
+}
