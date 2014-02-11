1dee161de3cfc5543fd2df63f5211f518e253eb8
Rainer Poisel <rainer.poisel@gmail.com>
2014-02-11 14:58:11 +0100

    i2c example extended.

--- wiringPi/i2c/i2c_test.c
+++ wiringPi/i2c/i2c_test.c
@@ -1,13 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #include <wiringPi.h>
+#include <wiringPiI2C.h>
 
 #define BASE_8574 0x70
 #define ADDR_I2C_8574 0x38
 
 int main(void)
 {
-    printf("Hello World!\n");
+    int fd = -1;
+    int value = 0;
+
+    if ((fd = wiringPiI2CSetup(ADDR_I2C_8574)) < 0)
+    {
+        return EXIT_FAILURE;
+    }
+
+    /* determine original value of outputs */
+    value = wiringPiI2CRead(fd);
+
+    /* all outputs LOW */
+    wiringPiI2CWrite(fd, 0xff);
+
+    delay(1000);
+
+    /* all outputs back to original value */
+    wiringPiI2CWrite(fd, value);
+
     return EXIT_SUCCESS;
 }
